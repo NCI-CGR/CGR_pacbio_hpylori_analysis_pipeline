230cbdfb014759a46cf274ec384e9fb2f7099ebf
Wen Luo <wen.luo@nih.gov>
2025-08-13 16:00:25 -0400

    update to slurm env submision

--- step2_running_hgap.sh
+++ step2_running_hgap.sh
@@ -7,20 +7,20 @@ MANIFEST=$1
 for i in  $(awk -F "\t" '{if(NR>1){print $1"_"$2"_"$3}}' $MANIFEST );do
 	VIAL_ID=$(echo $i | cut -f1 -d_)
 	#HGAP_ANALYSISID=$(awk -F "\t" -v vial=$VIAL_ID '{if($1==vial){print $3}}' $MANIFEST)
-	HGAP_ANALYSISID=$(echo $i | cut -f3 -d_)
+	HGAP_ANALYSISID=$(echo $i | awk -F"_" '{print $NF}')
 	LOG_DIR=${ROOT_DIR}/${i}/logs
 	OUT_HGAP=${ROOT_DIR}/${i}/HGAP_run/processed_hgap
 	OUT_HIFI=${ROOT_DIR}/${i}/deepconsensus/processed_hgap
 	REPORT_DIR=${ROOT_DIR}/${i}/Report/temp
 	STEP2_WORKING_FLAG=${LOG_DIR}/step2_circlator.working
 	STEP2_DONE_FLAG=${LOG_DIR}/step2_circlator.done
-	
+	rm $STEP2_WORKING_FLAG
 	mkdir -p $OUT_HGAP 2>/dev/null
-	mkdir -p $OUT_HIFI 2>/dev/null
+	#mkdir -p $OUT_HIFI 2>/dev/null
 	mkdir -p $LOG_DIR 2>/dev/null
 	mkdir -p $REPORT_DIR 2>/dev/null
 	mkdir -p ${ROOT_DIR}/${i}/HGAP_run/NUCMER 2>/dev/null
-	rm ${LOG_DIR}/step2_circlator_${HGAP_ANALYSISID}.std* 2>/dev/null
+	#rm ${LOG_DIR}/step2_circlator_${HGAP_ANALYSISID}.std* 2>/dev/null
 	
 	
 	if [[ -f ${STEP2_WORKING_FLAG} || -f ${STEP2_DONE_FLAG} ]];then
@@ -28,7 +28,8 @@ for i in  $(awk -F "\t" '{if(NR>1){print $1"_"$2"_"$3}}' $MANIFEST );do
 	
 	else	
 		touch ${STEP2_WORKING_FLAG}
-		CMD="qsub -cwd -q $QUEUE -pe by_node 2 -N step2_circlator_${HGAP_ANALYSISID} -e ${LOG_DIR}/step2_circlator_${HGAP_ANALYSISID}.stderr -o ${LOG_DIR}/step2_circlator_${HGAP_ANALYSISID}.stdout -S /bin/sh step2_running_hgap_single.sh $i $HGAP_ANALYSISID"
+		#CMD="qsub -cwd -q $QUEUE -pe by_node 2 -N step2_circlator_${HGAP_ANALYSISID} -e ${LOG_DIR}/step2_circlator_${HGAP_ANALYSISID}.stderr -o ${LOG_DIR}/step2_circlator_${HGAP_ANALYSISID}.stdout -S /bin/sh step2_running_hgap_single.sh $i $HGAP_ANALYSISID"
+		CMD="sbatch -J step2_circlator_${HGAP_ANALYSISID} -p cgrq --cpus-per-task 12 --time 196:00:00 -e ${LOG_DIR}/step2_circlator_${HGAP_ANALYSISID}.stderr -o ${LOG_DIR}/step2_circlator_${HGAP_ANALYSISID}.stdout step2_running_hgap_single.sh $i $HGAP_ANALYSISID"
 		echo $CMD >> NOHUPS/nohup_step2_$(date +\%Y\%m\%d).txt
 		echo $CMD
 		eval $CMD
--- step3_format_hgap_result_batch.sh
+++ step3_format_hgap_result_batch.sh
@@ -5,11 +5,11 @@ MANIFEST=$1
 
 
 for i in  $(awk -F "\t" '{if(NR>1){print $1"_"$2"_"$3}}' $MANIFEST);do
-	VIAL_ID=$(echo $i | cut -f1 -d_)
+	VIAL_ID=$(echo $i | rev | cut -f3- -d_ | rev)
 	#HGAP_ANALYSISID=$(awk -F "\t" -v vial=$VIAL_ID '{if($1==vial){print $3}}' $MANIFEST)
 	#CGR_ID=$(awk -F "\t" -v vial=$VIAL_ID '{if($1==vial){print $2}}' $MANIFEST)
-	HGAP_ANALYSISID=$(echo $i | cut -f3 -d_)
-	CGR_ID=$(echo $i | cut -f2 -d_)
+	HGAP_ANALYSISID=$(echo $i | awk -F"_" '{print $NF}')
+	CGR_ID=$(echo $i | awk -F"_" '{print $(NF-1)}')
 	LOG_DIR=${ROOT_DIR}/${i}/logs
 	STEP3_WORKING_FLAG=${LOG_DIR}/step3_format_hgap.working
 	STEP3_DONE_FLAG=${LOG_DIR}/step3_format_hgap.done
@@ -31,26 +31,26 @@ for i in  $(awk -F "\t" '{if(NR>1){print $1"_"$2"_"$3}}' $MANIFEST);do
 	#OUT_HGAP=${ROOT_DIR}/HGAP_file${BARCODING_JOB}_${i}_${i}
 	
 	#HAS_CIRLULARIZED=$(grep 'Circularized: yes' ${ROOT_DIR}/${i}/HGAP_run/circlator_${HGAP_ANALYSISID}/04.merge.circularise_details.log | wc -l);
-	
+	rm ${STEP3_WORKING_FLAG}
 	if [[ -f ${STEP3_WORKING_FLAG} ]] || [[ -f ${STEP3_DONE_FLAG} ]];then
 		echo "${i} format hgap assembly job is still running or already done, skip..."
 	else
-		rm -f $LOG_DIR/step3_format_hgap_*.std* 2>/dev/null
+		#rm -f $LOG_DIR/step3_format_hgap_*.std* 2>/dev/null
  
-		CMD="qsub -cwd -q $QUEUE -N Step3_format_hgap_${HGAP_ANALYSISID} -e ${LOG_DIR}/step3_format_hgap_${HGAP_ANALYSISID}.stderr -o ${LOG_DIR}/step3_format_hgap_${HGAP_ANALYSISID}.stdout -S /bin/sh step3_format_hgap_result_V2.sh ${ROOT_DIR}/${i}/HGAP_run/circlator_${HGAP_ANALYSISID}/05.clean.fasta"
-		#CMD="sbatch -e ${LOG_DIR}/step3_format_hgap_${HGAP_ANALYSISID}.stderr -o ${LOG_DIR}/step3_format_hgap_${HGAP_ANALYSISID}.stdout step3_format_hgap_result_V2.sh ${ROOT_DIR}/${i}/HGAP_run/circlator_${HGAP_ANALYSISID}/05.clean.fasta"
+		#CMD="qsub -cwd -q $QUEUE -N Step3_format_hgap_${HGAP_ANALYSISID} -e ${LOG_DIR}/step3_format_hgap_${HGAP_ANALYSISID}.stderr -o ${LOG_DIR}/step3_format_hgap_${HGAP_ANALYSISID}.stdout -S /bin/sh step3_format_hgap_result_V2.sh ${ROOT_DIR}/${i}/HGAP_run/circlator_${HGAP_ANALYSISID}/05.clean.fasta"
+		CMD="sbatch --time 96:00:00 -e ${LOG_DIR}/step3_format_hgap_${HGAP_ANALYSISID}.stderr -o ${LOG_DIR}/step3_format_hgap_${HGAP_ANALYSISID}.stdout step3_format_hgap_result_V2.sh ${ROOT_DIR}/${i}/HGAP_run/circlator_${HGAP_ANALYSISID}/05.clean.fasta"
 		echo $CMD >>  NOHUPS/nohup_step3_$(date +\%Y\%m\%d).txt
 		echo $CMD
 		eval $CMD
 
 	fi
-	
+	rm ${LOG_DIR}/step3_format_hifiasm.working
 	if [[ -f ${LOG_DIR}/step3_format_hifiasm.working ]] || [[ -f ${LOG_DIR}/step3_format_hifiasm.done ]];then
 		echo "${i} format hifiasm assembly job is still running or already done, skip..."
 	else			
-		rm -f $LOG_DIR/step3_format_hifiasm_*.std* 2>/dev/null
-		CMD="qsub -cwd -q $QUEUE -N Step3_format_hifiasm_${HGAP_ANALYSISID} -e ${LOG_DIR}/step3_format_hifiasm_${HGAP_ANALYSISID}.stderr -o ${LOG_DIR}/step3_format_hifiasm_${HGAP_ANALYSISID}.stdout -S /bin/sh step3_format_hgap_result_hifiasm.sh ${ROOT_DIR}/${i}/deepconsensus/circlator/05.clean.fasta"
-		#CMD="sbatch -e ${LOG_DIR}/step3_format_hifiasm_${HGAP_ANALYSISID}.stderr -o ${LOG_DIR}/step3_format_hifiasm_${HGAP_ANALYSISID}.stdout step3_format_hgap_result_hifiasm.sh ${ROOT_DIR}/${i}/deepconsensus/circlator/05.clean.fasta"
+
+		#CMD="qsub -cwd -q $QUEUE -N Step3_format_hifiasm_${HGAP_ANALYSISID} -e ${LOG_DIR}/step3_format_hifiasm_${HGAP_ANALYSISID}.stderr -o ${LOG_DIR}/step3_format_hifiasm_${HGAP_ANALYSISID}.stdout -S /bin/sh step3_format_hgap_result_hifiasm.sh ${ROOT_DIR}/${i}/hifiasm_run/circlator/05.clean.fasta"
+		CMD="sbatch --time 96:00:00 -e ${LOG_DIR}/step3_format_hifiasm_${HGAP_ANALYSISID}.stderr -o ${LOG_DIR}/step3_format_hifiasm_${HGAP_ANALYSISID}.stdout step3_format_hgap_result_hifiasm.sh ${ROOT_DIR}/${i}/hifiasm_run/circlator/05.clean.fasta"
 		echo $CMD >>  NOHUPS/nohup_step3_$(date +\%Y\%m\%d).txt
 		echo $CMD
 		eval $CMD
